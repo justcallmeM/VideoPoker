cd6e7774494e1f33d32bbe0a1d60a5386a27fb6f
Mindaugas Pikelis <36482664+justcallmeM@users.noreply.github.com>
2019-06-16 20:14:51 +0300

    comments

--- VideoPoker/HandTester/HandEvaluationTest.cs
+++ VideoPoker/HandTester/HandEvaluationTest.cs
@@ -9,10 +9,12 @@ namespace HandTester
         //Arrange
         [Theory]
         [InlineData(new string[] { "HA", "HQ", "HT", "HK", "HJ" }, "royal flush")]
+        //straight flush has to return false, because the logic is incomplete.
         [InlineData(new string[] { "H2", "H3", "H4", "H5", "H6" }, "straight flush")]
         [InlineData(new string[] { "HA", "DA", "CA", "SA", "HJ" }, "four of a kind")]
         [InlineData(new string[] { "H5", "D5", "H7", "C5", "D7" }, "full house")]
         [InlineData(new string[] { "HA", "HQ", "HT", "H9", "H5" }, "flush")]
+        //straight has to return false, because the logic is incomplete.
         [InlineData(new string[] { "HA", "CQ", "ST", "SK", "DJ" }, "straight")]
         [InlineData(new string[] { "HA", "DA", "CA", "HK", "HJ" }, "three of a kind")]
         [InlineData(new string[] { "HA", "DA", "HT", "DT", "HJ" }, "two pair")]
